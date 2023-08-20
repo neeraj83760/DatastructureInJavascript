2566cfd18c1bee4d67dbe63a1a97c574a91c12db
neeraj <neerajkumarghildiyal@gmail.com>
2023-08-21 01:02:47 +0530

    Added GFG mustDOproblems folder in this repo

--- practice.js
+++ practice.js
@@ -1,80 +1,60 @@
-// let arr = [7,8,9,1,2,4,4,4,4,2,7,3]
+class Node{
 
-// let string = ['neeraj', 'johny', 'sameer', 'sunil','rahul', 'neeraj','johny']
+    constructor(val){
 
-// let str = 'Ramesh' 
+        this.val = val;
+        this.next = null;
 
-// using of map function 
-// let Mul2Arry = arr.map(mul => mul*2) 
+        
+    }
+}
 
-// console.log(Mul2Arry);
-// using of spread operators 
+class Linkedlist {
 
-// console.log([...arr, ...Mul2Arry]); 
+    constructor()
+    {
+        this.head = null;
+    }
 
-// for(let s of arr){
+    insert(val){
 
-//     console.log(s);
-// }
+        if(this.head === null)
+        { 
+        this.head = new Node(val);
+        return;
+        }
 
-    // for(let s in arr){
+        let curr = this.head;
+        while(curr.next !== null){
 
-    //     console.log(s); // it will return where the indexes where placed
-    //     // console.log(arr[s]); 
-    // }
+            curr = curr.next;
+
+        }
+
+        curr.next = new Node(val);
+    }
 
 
-// let s = new Set(arr);
-// let s = new Set(string);
+    display(){
 
-// s = arr;
+        let root = this.head;
+        console.log('Inside this keyword :', this);
+        while(root !== null){
+        console.log('Inside root :' + JSON.stringify(root));    
+        console.log('Inside root.val  :' + root.val);
+        console.log('Inside root.next  :' + JSON.stringify(root.next));  // Inside next [object object]
+        root = root.next;
+        }
+    }
+}
+let a = new Linkedlist();
+a.insert(23);
+a.insert(3);
+a.insert(31);
+// a.insert(21);
+// a.insert(4);
+// a.insert(6);
 
-// console.log(...s); 
+a.display();
 
-// console.log(string[1].length);
-// console.log(string[2].charAt(5));
 
-// Below program is to reverse the srting : which is actaully an Incorrect code :)
-
-// let strRev =[]
-// for(let i=0; i<=str.length; i++){
-
-// strRev = str[str.length - i];
-// strRev[i++]; 
-// }
-
-// console.log(strRev);
-
-//***********************************/
-
-// const table = Array(3).fill(7)
-
-// const newArray = [3]; 
-
-// // console.log(table); 
-// console.log(newArray)
-
-// console.log(sizeof(table)); 
-
-// Learn how a map/dictionary/hash table works underneath the hood by implementing your 
-//own version in JavaScript.
-
-// const person = {}
-
-// person['fname'] = 'jonhy'
-// person['lname'] =  'sins'
-
-// console.log(person);
-//*********************************************************** */
-
-// 'use-strict';
-
-const ps = require("prompt-sync");
-// const prompt = ps();
-const prompt = ps({sigint: true}) // argument sigint shows that if the 
-// intrrupt signal is generated then treat it accordingly ..for example
-// if user is not entering any name and directly press ctrl+d 
-// then the program terminates accordingly ...that we can proceed accordingly 
-
-let name = prompt("Enter your name");
-console.log(`hello ${name}`)
